cc9a769130e8918a09cbd6de6d8ae3605c6e395e
hndrkk <ky0n@users.noreply.github.com>
2022-08-23 16:39:08 +0200

    set csv file to example.csv

--- main.py
+++ main.py
@@ -378,6 +378,7 @@ def wilcoxon_holm(alpha=0.05, df_perf=None):
     # return the p-values and the average ranks
     return p_values, average_ranks, max_nb_datasets
 
-df_perf = pd.read_csv('DefaultvsTunedvsEnsembleCritDiffAcc.csv',index_col=False)
+
+df_perf = pd.read_csv('example.csv', index_col=False)
 
 draw_cd_diagram(df_perf=df_perf, title='Accuracy', labels=True)
